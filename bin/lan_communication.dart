1ad92054385011714e9504b6ba1950c23198b518
Randy Kwalar <rkwalar@gmail.com>
2022-04-22 02:53:37 +0100

    Use long running isolates

--- bin/lan_communication.dart
+++ bin/lan_communication.dart
@@ -1,50 +1,59 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
+
+import 'package:async/async.dart';
 
 import 'package:lan_communication/parent_socket.dart';
 import 'package:lan_communication/server_socket.dart';
 import 'package:lan_communication/setup.dart';
 import 'package:lan_communication/client_socket.dart';
 
-Future<void> _parseInBackground(bool isServer) async {
+Future<void> _parseInBackground(bool isServer, Stream<String> commands) async {
   final p = ReceivePort();
-  if (isServer) {
-    await Isolate.spawn(_startServer, p.sendPort);
-  } else {
-    await Isolate.spawn(_startClient, p.sendPort);
+  await Isolate.spawn(_startSocket, p.sendPort);
+  final events = StreamQueue<dynamic>(p);
+  SendPort sendPort = await events.next;
+  sendPort.send(isServer);
+  commands.listen((event) async {
+    sendPort.send(event);
+    if (event == 'stop') {
+      await events.cancel();
+    }
+  });
+}
+
+void _startSocket(SendPort p) async {
+  final rPort2 = ReceivePort();
+  p.send(rPort2.sendPort);
+
+  late ParentSocket socket;
+  String ipAddress = await Setup.getIpAddress();
+
+  await for (final message in rPort2) {
+    if (message is bool) {
+      if (message == true) {
+        socket = ServerSocketClass();
+      } else {
+        socket = ClientSocketClass();
+      }
+      await socket.start(ipAddress);
+    } else if (message != 'stop') {
+      socket.sendMessage(message);
+    } else {
+      break;
+    }
+    Isolate.exit();
   }
-}
-
-void _startClient(SendPort p) async {
-  late ParentSocket socket;
-  String ipAddress = await Setup.getIpAddress();
-  socket = ClientSocketClass();
-  await socket.start(ipAddress);
-}
-
-void _startServer(SendPort p) async {
-  late ParentSocket socket;
-  String ipAddress = await Setup.getIpAddress();
-  socket = ServerSocketClass();
-  await socket.start(ipAddress);
 }
 
 void main(List<String> arguments) async {
   await Setup.start();
 
-  late ParentSocket socket;
-  String ipAddress = await Setup.getIpAddress();
-
   bool isServer = await Setup.isServer();
-  if (isServer) {
-    socket = ServerSocketClass();
-  } else {
-    socket = ClientSocketClass();
-  }
-
-  await socket.start(ipAddress);
-
-  await _parseInBackground(isServer);
+  final commandsStreamController = StreamController<String>();
+  String ipAddress = await Setup.getIpAddress();
+  await _parseInBackground(isServer, commandsStreamController.stream);
 
   String? input;
 
@@ -56,9 +65,10 @@ void main(List<String> arguments) async {
       break;
     }
     input = '$input\nFrom $ipAddress';
-    socket.sendMessage(input);
+    commandsStreamController.sink.add(input);
   }
 
-  await socket.stop();
+  commandsStreamController.sink.add('stop');
+  // await socket.stop();
   exit(0);
 }
