0ec44fe7a5aab4544e11d280cab57f57526f81b6
janWICKEDpro <janroyal45@gmail.com>
2022-04-23 11:59:53 +0100

    ceasars  cipher

--- lib/caesars_cipher.dart
+++ lib/caesars_cipher.dart
@@ -1,9 +1,18 @@
 class CaesarsCipher {
   static String encrypt({required String message, required int key}) {
-    return '';
+    String encryptedMessage = '';
+    for (var i = 0; i < message.length; i++) {
+      //Using unicodes.
+      encryptedMessage += String.fromCharCode(message.codeUnits[i] + key);
+    }
+    return encryptedMessage;
   }
 
   static String decrypt({required String message, required int key}) {
-    return '';
+    String decryptedMessage = '';
+    for (var i = 0; i < message.length; i++) {
+      decryptedMessage += String.fromCharCode(message.codeUnits[i] - key);
+    }
+    return decryptedMessage;
   }
 }
